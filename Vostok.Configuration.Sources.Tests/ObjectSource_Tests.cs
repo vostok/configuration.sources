46dec75f6ec6762d07f89204a9b3570e042c7a96
macart <macart@skbkontur.ru>
2019-09-12 13:27:05 +0500

    added ObjectSource

---
+++ Vostok.Configuration.Sources.Tests/ObjectSource_Tests.cs
@@ -0,0 +1,321 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using FluentAssertions.Extensions;
+using NUnit.Framework;
+using Vostok.Commons.Testing.Observable;
+using Vostok.Configuration.Abstractions.SettingsTree;
+using Vostok.Configuration.Sources.Object;
+
+namespace Vostok.Configuration.Sources.Tests
+{
+    [TestFixture]
+    internal class ObjectSource_Tests
+    {
+        [Test]
+        public void Should_return_null_settings_for_null_object()
+        {
+            Observe(null).Should().BeNull();
+        }
+
+        [Test]
+        public void Should_return_empty_object_node_for_empty_object()
+        {
+            Observe(new object()).Should().Be(new ObjectNode(Array.Empty<ISettingsNode>()));
+        }
+
+        [Test]
+        public void Should_return_empty_array_node_for_empty_sequence()
+        {
+            Observe(Array.Empty<int>()).Should().Be(new ArrayNode(Array.Empty<ISettingsNode>()));
+            Observe(new List<string>()).Should().Be(new ArrayNode(new List<ISettingsNode>()));
+        }
+
+        [Test]
+        public void Should_return_value_node_for_value_object()
+        {
+            Observe("string").Should().Be(new ValueNode("string"));
+            Observe(Guid.Empty).Should().Be(new ValueNode(Guid.Empty.ToString()));
+            Observe(100).Should().Be(new ValueNode("100"));
+        }
+
+        [Test]
+        public void Should_handle_dictionaries()
+        {
+            var dictionary1 = new Dictionary<string, TimeSpan>
+            {
+                {"1", TimeSpan.Zero},
+                {"2", TimeSpan.MinValue},
+                {"3", TimeSpan.MaxValue}
+            };
+            Observe(dictionary1)
+                .Should()
+                .Be(
+                    new ObjectNode(
+                        new[]
+                        {
+                            new ValueNode("1", TimeSpan.Zero.ToString()),
+                            new ValueNode("2", TimeSpan.MinValue.ToString()),
+                            new ValueNode("3", TimeSpan.MaxValue.ToString())
+                        }));
+
+            var dictionary2 = new Dictionary<int, string>
+            {
+                {1, "2"},
+                {3, "4"},
+                {5, "6"}
+            };
+            Observe(dictionary2)
+                .Should()
+                .Be(
+                    new ObjectNode(
+                        new[]
+                        {
+                            new ValueNode("1", "2"),
+                            new ValueNode("3", "4"),
+                            new ValueNode("5", "6")
+                        }));
+        }
+
+        [Test]
+        public void Should_handle_sequences()
+        {
+            Observe(Enumerable.Range(1, 5).ToArray())
+                .Should()
+                .Be(new ArrayNode(Enumerable.Range(1, 5).Select(i => new ValueNode(i.ToString())).ToArray()));
+            Observe(Enumerable.Range(1, 10).ToList())
+                .Should()
+                .Be(new ArrayNode(Enumerable.Range(1, 10).Select(i => new ValueNode(i.ToString())).ToArray()));
+            Observe(Enumerable.Repeat(Guid.Empty, 1).ToArray())
+                .Should()
+                .Be(new ArrayNode(Enumerable.Repeat(Guid.Empty, 1).Select(i => new ValueNode(i.ToString())).ToArray()));
+        }
+
+        [Test]
+        public void Should_handle_complex_objects()
+        {
+            var johnDoe = new Person("John Doe", 65);
+            var judyDoe = new Person("Judy Doe", 22);
+            var jamesDoe = new Person("James Doe", 40);
+
+            johnDoe.Children.Add(judyDoe);
+            johnDoe.Children.Add(jamesDoe);
+
+
+            johnDoe.Info.Add(PersonInfo.BirthDayDate, "Date");
+            judyDoe.Info.Add(PersonInfo.SocialSecurityNumber, "Number");
+
+            Observe(johnDoe)
+                .Should()
+                .Be(
+                    new ObjectNode(
+                        new ISettingsNode[]
+                        {
+                            new ValueNode("Age", "65"),
+                            new ValueNode("Name", "John Doe"),
+                            new ArrayNode(
+                                "Children",
+                                new ISettingsNode[]
+                                {
+                                    new ObjectNode(
+                                        new ISettingsNode[]
+                                        {
+                                            new ValueNode("Age", "22"),
+                                            new ValueNode("Name", "Judy Doe"),
+                                            new ArrayNode("Children", Array.Empty<ISettingsNode>()),
+                                            new ObjectNode(
+                                                "Info",
+                                                new ISettingsNode[]
+                                                {
+                                                    new ValueNode("SocialSecurityNumber", "Number")
+                                                })
+                                        }),
+                                    new ObjectNode(
+                                        new ISettingsNode[]
+                                        {
+                                            new ValueNode("Age", "40"),
+                                            new ValueNode("Name", "James Doe"),
+                                            new ArrayNode("Children", Array.Empty<ISettingsNode>()),
+                                            new ObjectNode("Info", Array.Empty<ISettingsNode>())
+                                        })
+                                }),
+                            new ObjectNode(
+                                "Info",
+                                new ISettingsNode[]
+                                {
+                                    new ValueNode("BirthDayDate", "Date")
+                                }),
+                        }));
+        }
+
+        [Test]
+        public void Should_hanlde_sequence_of_dictionaries()
+        {
+            var arrayOfDictionaries = new[]
+            {
+                new Dictionary<string, int>
+                {
+                    {"100", 100},
+                    {"200", 200}
+                },
+                new Dictionary<string, int>
+                {
+                    {"1000", 1000}
+                },
+            };
+
+            Observe(arrayOfDictionaries)
+                .Should()
+                .Be(
+                    new ArrayNode(
+                        null,
+                        new ISettingsNode[]
+                        {
+                            new ObjectNode(
+                                null,
+                                new List<ISettingsNode>
+                                {
+                                    new ValueNode("100", "100"),
+                                    new ValueNode("200", "200")
+                                }),
+                            new ObjectNode(
+                                null,
+                                new List<ISettingsNode>
+                                {
+                                    new ValueNode("1000", "1000"),
+                                })
+                        }));
+        }
+
+        [Test]
+
+        public void Should_handle_dictionary_of_objects()
+        {
+            var dictionaryOfPersons = new Dictionary<string, Person>
+            {
+                ["Father"] = new Person("John Doe", 65),
+                ["Daughter"] = new Person("Judy Doe", 22),
+                ["Son"] = new Person("James Doe", 40)
+            };
+
+            Observe(dictionaryOfPersons)
+                .Should()
+                .Be(
+                    new ObjectNode(
+                        new ISettingsNode[]
+                        {
+                            new ObjectNode(
+                                "Father",
+                                new ISettingsNode[]
+                                {
+                                    new ValueNode("Age", "65"),
+                                    new ValueNode("Name", "John Doe"),
+                                    new ArrayNode("Children", Array.Empty<ISettingsNode>()),
+                                    new ObjectNode("Info", Array.Empty<ISettingsNode>())
+                                }),
+                            new ObjectNode(
+                                "Daughter",
+                                new ISettingsNode[]
+                                {
+                                    new ValueNode("Age", "22"),
+                                    new ValueNode("Name", "Judy Doe"),
+                                    new ArrayNode("Children", Array.Empty<ISettingsNode>()),
+                                    new ObjectNode("Info", Array.Empty<ISettingsNode>())
+
+                                }),
+                            new ObjectNode(
+                                "Son",
+                                new ISettingsNode[]
+                                {
+                                    new ValueNode("Age", "40"),
+                                    new ValueNode("Name", "James Doe"),
+                                    new ArrayNode("Children", Array.Empty<ISettingsNode>()),
+                                    new ObjectNode("Info", Array.Empty<ISettingsNode>())
+                                }),
+                        }));
+        }
+
+        [Test]
+        public void Should_error_on_cyclic_dependency()
+        {
+            var johnDoe = new Person("John Doe", 65);
+            var judyDoe = new Person("Judy Doe", 22);
+            var jamesDoe = new Person("James Doe", 40);
+
+            johnDoe.Children.Add(judyDoe);
+            judyDoe.Children.Add(jamesDoe);
+            jamesDoe.Children.Add(johnDoe);
+
+            var error = new ObjectSource(johnDoe).Observe().WaitFirstValue(1.Seconds()).error;
+            error.Should().NotBeNull().And.BeOfType<ArgumentException>();
+        }
+
+        [Test]
+        public void Should_handle_objects_with_repeated_elements()
+        {
+            var listOfInts = new List<int> {1, 2, 3};
+            var dictionaryOfLists = new Dictionary<string, IList<int>>
+            {
+                ["First"] = listOfInts,
+                ["Second"] = listOfInts,
+                ["Third"] = listOfInts
+            };
+
+            Observe(dictionaryOfLists)
+                .Should()
+                .Be(
+                    new ObjectNode(
+                        null,
+                        new ISettingsNode[]
+                        {
+                            new ArrayNode(
+                                "First",
+                                new List<ISettingsNode>
+                                {
+                                    new ValueNode("1"), new ValueNode("2"), new ValueNode("3")
+                                }),
+                            new ArrayNode(
+                                "Second",
+                                new List<ISettingsNode>
+                                {
+                                    new ValueNode("1"), new ValueNode("2"), new ValueNode("3")
+                                }),
+                            new ArrayNode(
+                                "Third",
+                                new List<ISettingsNode>
+                                {
+                                    new ValueNode("1"), new ValueNode("2"), new ValueNode("3")
+                                })
+                        }));
+        }
+
+        private static ISettingsNode Observe(object obj)
+        {
+            return new ObjectSource(obj).Observe().WaitFirstValue(1.Seconds()).settings;
+        }
+
+        private class Person
+        {
+            public string Name { get; }
+            public int Age;
+            public ICollection<Person> Children { get; }
+            public IDictionary<PersonInfo, string> Info { get; }
+
+            public Person(string name, int age)
+            {
+                Name = name;
+                Age = age;
+                Children = new List<Person>();
+                Info = new Dictionary<PersonInfo, string>();
+            }
+        }
+
+        private enum PersonInfo
+        {
+            BirthDayDate,
+            SocialSecurityNumber
+        }
+
+    }
+}
---
+++ Vostok.Configuration.Sources/Object/ObjectSource.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using Vostok.Configuration.Abstractions.SettingsTree;
+using Vostok.Configuration.Sources.Manual;
+using Vostok.Commons.Collections;
+using Vostok.Configuration.Extensions;
+using Vostok.Commons.Formatting;
+using System.Linq;
+using System.Text;
+
+namespace Vostok.Configuration.Sources.Object
+{
+    /// <summary>
+    /// <para>A source which returns settings from the POCO object provided by user.</para>
+    /// <para>POCO object should contain simple properties and fields like dictionaries, </para>
+    /// <para>lists, arrays and other POCO objects. Keys of dictionaries should be of </para>
+    /// <para>primitive types, enums, strings or Guids.</para>
+    /// </summary>
+    public class ObjectSource : ManualFeedSource<object>
+    {
+        private static readonly Dictionary<Type, Func<object, string>> CustomFormatters
+            = new Dictionary<Type, Func<object, string>>
+            {
+                [typeof(Encoding)] = value => ((Encoding)value).WebName
+            };
+
+        public ObjectSource()
+            : base(obj => ParseObject(null, obj, new HashSet<object>(ByReferenceEqualityComparer<object>.Instance)))
+        {
+        }
+
+        public ObjectSource([CanBeNull] object source)
+            : this()
+        {
+            Push(source);
+        }
+
+        private static ISettingsNode ParseObject([CanBeNull] string name, [CanBeNull] object item, [NotNull] HashSet<object> path)
+        {
+            if (item == null)
+                return null;
+
+            if (!path.Add(item))
+                throw new ArgumentException("Object has cyclic dependency.");
+
+            try
+            {
+                var itemType = item.GetType();
+
+                if (ToStringDetector.HasCustomToString(itemType))
+                    return new ValueNode(name, item.ToString());
+
+                foreach (var pair in CustomFormatters)
+                {
+                    if (pair.Key.IsAssignableFrom(itemType))
+                        return new ValueNode(name, pair.Value(item));
+                }
+
+                if (DictionaryInspector.IsSimpleDictionary(itemType))
+                    return ParseDictionary(name, DictionaryInspector.EnumerateSimpleDictionary(item), path);
+
+                if (item is IEnumerable sequence)
+                    return ParseEnumerable(name, sequence, path);
+
+                var fieldsAndProperties = new List<ISettingsNode>();
+
+                foreach (var field in itemType.GetInstanceFields())
+                    fieldsAndProperties.Add(ParseObject(field.Name, field.GetValue(item), path));
+
+                foreach (var property in itemType.GetInstanceProperties())
+                    fieldsAndProperties.Add(ParseObject(property.Name, property.GetValue(item), path));
+
+                return new ObjectNode(name, fieldsAndProperties);
+            }
+            finally
+            {
+                path.Remove(item);
+            }
+        }
+
+        private static ArrayNode ParseEnumerable(string name, IEnumerable sequence, HashSet<object> path)
+        {
+            var items = new List<ISettingsNode>();
+            foreach (var element in sequence)
+                items.Add(ParseObject(null, element, path));
+            return new ArrayNode(name, items);
+        }
+
+        private static ObjectNode ParseDictionary(string name, IEnumerable<(string, object)> pairs, HashSet<object> path)
+        {
+            var tokens = pairs.Select(pair => ParseObject(pair.Item1, pair.Item2, path)).ToArray();
+            return new ObjectNode(name, tokens);
+        }
+    }
+}
