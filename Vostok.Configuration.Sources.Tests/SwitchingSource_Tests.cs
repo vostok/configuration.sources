ac007a5b90e3fddc8d9ca4ed7239230b610bcb7f
iloktionov <iloktionov@skbkontur.ru>
2020-05-10 19:18:10 +0500

    Implemented SwitchingSource.

---
+++ Vostok.Configuration.Sources.Tests/SwitchingSource_Tests.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using NUnit.Framework;
+using Vostok.Configuration.Abstractions.SettingsTree;
+using Vostok.Configuration.Sources.Manual;
+using Vostok.Configuration.Sources.Switching;
+using ObservableExtensions = Vostok.Configuration.Abstractions.Extensions.Observable.ObservableExtensions;
+
+namespace Vostok.Configuration.Sources.Tests
+{
+    [TestFixture]
+    internal class SwitchingSource_Tests
+    {
+        [Test]
+        public void Should_switch_to_a_new_source_as_soon_as_it_becomes_available()
+        {
+            var source1 = CreateSource();
+            var source2 = CreateSource();
+            var source3 = CreateSource();
+            
+            var values = new List<int>();
+
+            var switchingSource = new SwitchingSource(source1);
+
+            using (ObservableExtensions.Subscribe(switchingSource.Observe(), pair => values.Add(int.Parse(pair.settings.Value ?? "0"))))
+            {
+                source1.Push(1);
+                source1.Push(2);
+                source1.Push(3);
+
+                source2.Push(4);
+                source2.Push(5);
+
+                switchingSource.SwitchTo(source2);
+
+                source1.Push(6);
+                source1.Push(7);
+
+                source2.Push(8);
+                source2.Push(9);
+
+                source3.Push(10);
+                source3.Push(11);
+
+                switchingSource.SwitchTo(source3);
+
+                source1.Push(12);
+                source1.Push(13);
+
+                source2.Push(14);
+                source2.Push(15);
+
+                source3.Push(16);
+                source3.Push(17);
+
+                switchingSource.SwitchTo(source1);
+                switchingSource.SwitchTo(source2);
+
+                switchingSource.SwitchTo(switchingSource.CurrentSource.Transform(node => new ValueNode(node.Value + "00")));
+            }
+
+            values.Should().Equal(1, 2, 3, 5, 8, 9, 11, 16, 17, 13, 15, 1500);
+        }
+
+        private static ManualFeedSource<int> CreateSource()
+            => new ManualFeedSource<int>(value => new ValueNode(value.ToString()));
+    }
+}
---
+++ Vostok.Configuration.Sources/Switching/SwitchingSource.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using JetBrains.Annotations;
+using Vostok.Configuration.Abstractions;
+using Vostok.Configuration.Abstractions.SettingsTree;
+
+namespace Vostok.Configuration.Sources.Switching
+{
+    /// <summary>
+    /// A source that proxies from an underlying source that can be dynamically swapped to another one without resubscribing.
+    /// </summary>
+    [PublicAPI]
+    public class SwitchingSource : IConfigurationSource
+    {
+        private readonly ReplaySubject<IConfigurationSource> sources = new ReplaySubject<IConfigurationSource>(1);
+
+        private volatile IConfigurationSource currentSource;
+
+        public SwitchingSource([NotNull] IConfigurationSource initialSource)
+            => SwitchTo(initialSource);
+
+        public IConfigurationSource CurrentSource => currentSource;
+
+        public void SwitchTo(IConfigurationSource newSource)
+            => sources.OnNext(currentSource = newSource);
+
+        public void SwitchTo(Func<IConfigurationSource, IConfigurationSource> transform)
+            => SwitchTo(transform(currentSource));
+
+        public IObservable<(ISettingsNode settings, Exception error)> Observe()
+            => sources.Select(source => source.Observe()).Switch();
+    }
+}
