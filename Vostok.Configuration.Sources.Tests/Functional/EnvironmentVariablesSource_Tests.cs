7f40ec6f7c448384d038ede5d5630dca468ac492
kungurtsev <kungurtsev@skbkontur.ru>
2021-11-25 16:14:05 +0500

    fix test

--- Vostok.Configuration.Sources.Tests/Functional/EnvironmentVariablesSource_Tests.cs
+++ Vostok.Configuration.Sources.Tests/Functional/EnvironmentVariablesSource_Tests.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Diagnostics;
-using System.Linq;
-using System.Runtime.InteropServices;
-using FluentAssertions;
+﻿using FluentAssertions;
 using FluentAssertions.Extensions;
 using NUnit.Framework;
 using Vostok.Commons.Testing.Observable;
-using Vostok.Configuration.Abstractions.SettingsTree;
 using Vostok.Configuration.Sources.Environment;
 
 namespace Vostok.Configuration.Sources.Tests.Functional
@@ -20,24 +15,7 @@ namespace Vostok.Configuration.Sources.Tests.Functional
             var evs = new EnvironmentVariablesSource();
             var res = evs.Observe().WaitFirstValue(100.Milliseconds()).settings;
 
-            CheckResult(res);
-        }
-
-        private static void CheckResult(ISettingsNode settings)
-        {
-            foreach (var k in System.Environment.GetEnvironmentVariables().Keys)
-                Console.WriteLine(k);
-
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-            {
-                (settings["pAtH"]?.Value).Should().NotBeNull();
-                (settings["APPdata"]?.Value).Should().NotBeNull();
-            }
-            else
-            {
-                (settings["pAtH"]?.Value).Should().NotBeNull();
-                (settings["sheLL"]?.Value).Should().NotBeNull();
-            }
+            (res["DoTnET_RoOt"]?.Value).Should().NotBeNull();
         }
     }
 }
